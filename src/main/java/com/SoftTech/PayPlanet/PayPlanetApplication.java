533c6fab760fad8191f65afb65d25fe6de670e14
Olasubomi <shubbynlaa@gmail.com>
2023-07-11 02:04:25 +0100

    Initial commit.

---
+++ src/main/java/com/SoftTech/PayPlanet/PayPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.SoftTech.PayPlanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayPlanetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SoftTech/PayPlanet/PayPlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoftTech.PayPlanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayPlanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
