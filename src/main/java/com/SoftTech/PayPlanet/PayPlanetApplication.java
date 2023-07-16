76ee44d4ceb47452b8df172cd0b51a178d619388
Olasubomi <shubbynlaa@gmail.com>
2023-07-16 22:31:06 +0100

    Setting up application environments

--- src/main/java/com/SoftTech/PayPlanet/PayPlanetApplication.java
+++ src/main/java/com/SoftTech/PayPlanet/PayPlanetApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PayPlanetApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PayPlanetApplication.class, args);
 	}
-
 }
