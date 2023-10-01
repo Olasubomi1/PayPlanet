4696177bb4e2c6bf70c86068cc95978dd2dc00d4
Olasubomi <shubbynlaa@gmail.com>
2023-10-01 01:42:55 +0100

    Added a user signup payload class.

--- src/main/java/com/SoftTech/PayPlanet/modules/user/payload/request/SignupUserRequestPayload.java
+++ src/main/java/com/SoftTech/PayPlanet/modules/user/payload/request/SignupUserRequestPayload.java
@@ -1,4 +1,12 @@
 package com.SoftTech.PayPlanet.modules.user.payload.request;
 
+import lombok.Data;
+
+@Data
 public class SignupUserRequestPayload {
+    private String username;
+    private String mobileNumber;
+    private String emailAddress;
+    private String password;
+    private String gender;
 }
