65f58f22fcb842e2d129bae3530b5beb9377c812
Olasubomi <shubbynlaa@gmail.com>
2023-10-01 01:41:32 +0100

    Created a password util class and its implementation.

--- src/main/java/com/SoftTech/PayPlanet/utils/PasswordUtil.java
+++ src/main/java/com/SoftTech/PayPlanet/utils/PasswordUtil.java
@@ -1,4 +1,17 @@
 package com.SoftTech.PayPlanet.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 public class PasswordUtil {
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public String hashPassword(String rawPassword){
+        return passwordEncoder.encode(rawPassword);
+    }
+
+    public boolean isPasswordMatch(String rawPassword, String encodedPassword){
+        return passwordEncoder.matches(rawPassword, encodedPassword);
+    }
 }
