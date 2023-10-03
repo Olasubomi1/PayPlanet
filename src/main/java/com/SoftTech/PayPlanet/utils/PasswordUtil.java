9ef0193e041c06c4483e2034c66756c4816d0ee3
Olasubomi <shubbynlaa@gmail.com>
2023-10-03 16:22:22 +0100

    Added implementation for JWT and PasswordEncoder utils.

--- src/main/java/com/SoftTech/PayPlanet/utils/PasswordUtil.java
+++ src/main/java/com/SoftTech/PayPlanet/utils/PasswordUtil.java
@@ -2,7 +2,9 @@ package com.SoftTech.PayPlanet.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PasswordUtil {
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
