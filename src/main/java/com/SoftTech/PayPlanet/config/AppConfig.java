d176ea382255de4700acc77bf69e0eb708176bc9
Olasubomi <shubbynlaa@gmail.com>
2023-10-12 22:55:34 +0100

    Added lombok logger to file.

--- src/main/java/com/SoftTech/PayPlanet/config/AppConfig.java
+++ src/main/java/com/SoftTech/PayPlanet/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.SoftTech.PayPlanet.config;
 
 import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
@@ -8,9 +9,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Properties;
+
 @Configuration
+@Slf4j
 public class AppConfig {
     @Autowired
     private Environment env;
@@ -47,4 +53,21 @@ public class AppConfig {
 //        messageSource.setCacheSeconds(0);
 //        return messageSource;
 //    }
+
+    @Bean
+    public JavaMailSender javaMailSender(){
+        Properties props = new Properties();
+        props.put("mail.smtp.starttls.enable", "true");
+        props.setProperty("mail.debug", "true");
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(env.getProperty("spring.mail.host"));
+        mailSender.setPort(587);
+        mailSender.setPassword(env.getProperty("spring.mail.password"));
+        mailSender.setUsername(env.getProperty("spring.mail.username"));
+        mailSender.setJavaMailProperties(props);
+
+//        mailSender.setProtocol("");
+
+        return mailSender;
+    }
 }
--- src/main/java/com/SoftTech/PayPlanet/modules/user/controller/PlanetUserController.java
+++ src/main/java/com/SoftTech/PayPlanet/modules/user/controller/PlanetUserController.java
@@ -3,6 +3,7 @@ package com.SoftTech.PayPlanet.modules.user.controller;
 import com.SoftTech.PayPlanet.dto.ServerResponse;
 import com.SoftTech.PayPlanet.modules.user.payload.request.SignupUserRequestPayload;
 import com.SoftTech.PayPlanet.modules.user.service.IPlanetUserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping(value = "api/user")
 public class PlanetUserController {
 
